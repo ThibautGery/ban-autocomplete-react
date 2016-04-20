8d37fab37d172aa70e18656914f5da5b8e81a03f
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-20 14:33:18 +0200

    use query to get result from BAN

--- src/client/app/AutocompleteBan.jsx
+++ src/client/app/AutocompleteBan.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 
+
 class AutocompleteBan extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = { query: "" };
+    this.handleChange = this.handleChange.bind(this)
+    this.getResults = this.getResults.bind(this)
   }
 
   getResults() {
-    const banRequest = new Request('http://api-adresse.data.gouv.fr/search/?q=8 bd du port');
+    const query = this.state.query
+    const banRequest = new Request('http://api-adresse.data.gouv.fr/search/?q=' + query);
     fetch(banRequest)
       .then(function(response) {
         return response.json()
       })
       .then(function(json) {
-        console.log('result', json)
+        console.log('result', json.features)
       });
   }
 
+  handleChange(event) {
+    console.log('event.target', event.target.value)
+    this.setState({ query: event.target.value });
+  }
 
   render() {
-    return (<input type="text" onKeyUp={this.getResults} />);
+    return (<div class="search">
+      <input  type="text"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                      onKeyUp={this.getResults} />
+
+    </div>);
   }
 
 }
