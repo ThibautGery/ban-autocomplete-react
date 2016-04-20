ff467879bca952e3f2c99f52b34f96242d062f73
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-20 11:59:21 +0200

    add awesome component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return (
+      <div>
+        <p> Hello React!</p>
+        <AwesomeComponent />
+      </div>);
   }
 }
 
