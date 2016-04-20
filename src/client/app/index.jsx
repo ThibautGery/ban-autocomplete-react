057e5926f53707a3a9500011de663f1709aac434
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-20 11:51:02 +0200

    init react

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,10 @@
-console.log('helloworld')
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <p> Hello React!</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
