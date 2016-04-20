dee85a09641f90c9b08fccffc06ee6b12833b574
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-20 12:03:07 +0200

    add autocomplete component

---
+++ src/client/app/AutocompleteBan.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AutocompleteBan extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (<input type="text" />);
+  }
+
+}
+
+export default AutocompleteBan;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
+import AutocompleteBan from './AutocompleteBan.jsx';
 
 class App extends React.Component {
   render () {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div>
         <p> Hello React!</p>
         <AwesomeComponent />
+        <AutocompleteBan />
       </div>);
   }
 }
