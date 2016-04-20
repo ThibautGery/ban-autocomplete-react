47d2a64bb9cbfc1d4cffd7bfffb8ea2918995c29
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-20 13:59:06 +0200

    add request to BAN on event

--- src/client/app/AutocompleteBan.jsx
+++ src/client/app/AutocompleteBan.jsx
@@ -7,7 +7,14 @@ class AutocompleteBan extends React.Component {
   }
 
   getResults() {
-    console.log('ON KEY UP PRESSED')
+    const banRequest = new Request('http://api-adresse.data.gouv.fr/search/?q=8 bd du port');
+    fetch(banRequest)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(json) {
+        console.log('result', json)
+      });
   }
 
 
