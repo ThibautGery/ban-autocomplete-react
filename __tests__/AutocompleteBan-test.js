c0ddd4a00b28c9d3cd75ba7d3dc7532c683f80f2
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-22 10:34:14 +0200

    add test on initialization

---
+++ __tests__/AutocompleteBan-test.js
@@ -0,0 +1,35 @@
+jest.unmock('../src/client/app/AutocompleteBan.jsx');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import AutocompleteBan from '../src/client/app/AutocompleteBan.jsx';
+
+describe('AutocompleteBan', () => {
+  let autocompleteBan;
+
+
+
+  describe('at init', () => {
+    beforeEach(() => {
+      autocompleteBan = TestUtils.renderIntoDocument(
+        <AutocompleteBan />
+      );
+    })
+
+    it('create a div', () => {
+      const autocompleteBanDom = ReactDOM.findDOMNode(autocompleteBan);
+      expect(autocompleteBanDom.tagName).toEqual('DIV');
+      expect(autocompleteBanDom.classList.length).toEqual(1);
+      expect(autocompleteBanDom.classList[0]).toEqual('ban-search');
+    })
+
+    it('the input search is empty', () => {
+      const autocompleteBanDom = ReactDOM.findDOMNode(autocompleteBan);
+      const input = autocompleteBanDom.children[0]
+      expect(input.tagName).toEqual('INPUT');
+      expect(input.value).toEqual('');
+      expect(input.classList[0]).toEqual('searchBox');
+    })
+  })
+});
